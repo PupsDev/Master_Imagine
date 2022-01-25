2c60300b5eba5d721500b898f66ed0ede76ded8d
PupsDev <Control3d@gmail.com>
2022-01-25 12:13:38 +0100

    tp2 + rapport

--- S2/HAI809I Codage et compression multimédia/tp2/src/main.cpp
+++ S2/HAI809I Codage et compression multimédia/tp2/src/main.cpp
@@ -34,14 +34,15 @@ int main(int argc, char* argv[]) {
     int nH, nW, nTaille;
     char actualpath [20+1];
     char *ptr;
+        int composante =3;
     if (argc != 3) 
     {
-        printf("Usage: ImageIn.ppm ImageOut.ppm  \n"); 
+        printf("Usage: ImageIn.ppm composante  \n"); 
         exit (1) ;
     }
 
     sscanf (argv[1],"%s",inputName) ;
-    sscanf (argv[2],"%s",outputName);
+    sscanf (argv[2],"%d",&composante);
 
     char * folderIn = (char*)"res/";
     char * folderOut = (char*)"out/"; 
@@ -52,7 +53,7 @@ int main(int argc, char* argv[]) {
     ptr = realpath(pathIn, actualpath);
     printf("input : %s \n", pathIn);
     printf("output : %s \n", pathOut);
-    //printf("%s \n", ptr);
+
 
     OCTET *ImgIn, *ImgTrans, *ImgOut;
 
@@ -65,59 +66,7 @@ int main(int argc, char* argv[]) {
     allocation_tableau(ImgOut, OCTET, nTaille3);
     allocation_tableau(ImgTrans, OCTET, nTaille3);
 
-    // SUBSAMBLING RGB
-    /*
-    for (int i=0; i < nTaille3; i+=3)
-    {
-        ImgOut[i  ]=ImgIn[i ];
-
-    }
-    for (int i=0; i < nTaille3; i+=6)
-    {
-        ImgTrans[i+1]=ImgIn[i+1]; 
-        ImgTrans[i+2]=ImgIn[i+2];
-    }
-    
-    for (int i=0; i < nTaille3; i+=6)
-    {
-        ImgOut[i+1]=ImgTrans[i+1]; 
-        ImgOut[i+2]=ImgTrans[i+2];
-        int k = i+=3;
-        ImgOut[k+1]=ImgTrans[i+1]; 
-        ImgOut[k+2]=ImgTrans[i+2];
-    }
-    */
-    // SUBSAMBLING CHROMILUMI
-    /*
-    OCTET *luminance, *chrom;
-    allocation_tableau(luminance, OCTET, nTaille3);
-    allocation_tableau(chrom, OCTET, nTaille3);
-
-    for (int i=0; i < nTaille3; i+=3)
-    {
-        luminance[i  ]=0.299*ImgIn[i ]+0.587*ImgIn[i+1]+0.114*ImgIn[i+2];
-
-    }
-    for (int i=0; i < nTaille3; i+=6)
-    {
-        //CB
-        chrom[i+2]= (ImgIn[i+2]-ImgOut[i])/1.772; 
-        //CR
-        chrom[i]= (ImgIn[i]-ImgOut[i])/1.402;
-        int k = i+=3;
-        chrom[k]=chrom[i]; 
-        chrom[k+2]=chrom[i+2];
-
-    }
-    for (int i=0; i < nTaille3; i+=3)
-    {
-        ImgOut[i+0]=  chrom[i]*1.772 + luminance[i ];
-        ImgOut[i+2]=  chrom[i+2]*1.402 + luminance[i ];
-        ImgOut[i+1] = (luminance[i ] -  ImgOut[i+0]*0.299 - ImgOut[i+2]*0.114)/0.587;
-
-    }
-
-*/
+  
     // SUBSAMBLING RGB
     std::vector<std::vector<couleur>> image;
     image.resize(nH);
@@ -136,85 +85,214 @@ int main(int argc, char* argv[]) {
     OCTET *ImgGB;
     allocation_tableau(ImgGB, OCTET, nTaille3);
 
-    bool RGB = false; 
-    if(RGB)
+
+    int tnW = 3*nW;
+
+
+    switch(composante)
     {
-            int tnW = 3*nW;
-    for (int i=0; i < nH; i++)
-    {
-        for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
-        {
-            image[i][j].r=ImgIn[i *tnW +p  ];
-            image[i][j].g=ImgIn[i *tnW +p+1];
-            image[i][j].b=ImgIn[i *tnW +p+2];
-
-
-            image2[i][j].r=ImgIn[i *tnW +p  ];
-
-        }
-    }
-    // SUBSAMBLING RGB
-    
-
-    for (int i=0; i < nH; i+=2)
-    {
-        for (int j=0; j < nW; j+=2)
-        { 
-
-            image3[i][j].g= image[i][j].g;
-            image3[i][j].b= image[i][j].b;
-
-        }
-
-    }
-    for (int i=0; i < nH; i+=2)
-    {
-        for (int j=0; j < nW; j+=2)
-        { 
-
-            for(int u = 0 ; u<2;u++)
-                for(int v =0 ; v<2;v++)
+        case 1:
+                for (int i=0; i < nH; i++)
                 {
-                    image3[i+u][j+v].g= image[i][j].g;
-                    image3[i+u][j+v].b= image[i][j].b;
+                    for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                    {
+                        image[i][j].r=ImgIn[i *tnW +p  ];
+                        image[i][j].g=ImgIn[i *tnW +p+1];
+                        image[i][j].b=ImgIn[i *tnW +p+2];
+
+
+                        image2[i][j].r=ImgIn[i *tnW +p  ];
+
+                    }
                 }
+                // SUBSAMBLING RGB
+                
 
-        }
+                for (int i=0; i < nH; i+=2)
+                {
+                    for (int j=0; j < nW; j+=2)
+                    { 
 
+                        image3[i][j].g= image[i][j].g;
+                        image3[i][j].b= image[i][j].b;
+
+                    }
+
+                }
+                for (int i=0; i < nH; i+=2)
+                {
+                    for (int j=0; j < nW; j+=2)
+                    { 
+
+                        for(int u = 0 ; u<2;u++)
+                            for(int v =0 ; v<2;v++)
+                            {
+                                image3[i+u][j+v].g= image[i][j].g;
+                                image3[i+u][j+v].b= image[i][j].b;
+                            }
+
+                    }
+
+                }
+                    for (int i=0; i < nH; i++)
+                {
+                    for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                    {
+                        ImgGB[i *tnW +p  ]=0;
+                        ImgGB[i *tnW +p+1]=image3[i][j].g;
+                        ImgGB[i *tnW +p+2]=image3[i][j].b;
+                    }
+                }
+                    // END SUBSAMBLING RGB
+            for (int i=0; i < nH; i++)
+            {
+                for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                {
+                    ImgOut[i *tnW +p  ]=image2[i][j].r;
+                    ImgOut[i *tnW +p+1]=image3[i][j].g;
+                    ImgOut[i *tnW +p+2]=image3[i][j].b;
+                }
+            }
+        break;
+        case 2:
+                for (int i=0; i < nH; i++)
+                {
+                    for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                    {
+                        image[i][j].r=ImgIn[i *tnW +p  ];
+                        image[i][j].g=ImgIn[i *tnW +p+1];
+                        image[i][j].b=ImgIn[i *tnW +p+2];
+
+
+                        image2[i][j].g=ImgIn[i *tnW +p+1];
+
+                    }
+                }
+                // SUBSAMBLING RGB
+                
+
+                for (int i=0; i < nH; i+=2)
+                {
+                    for (int j=0; j < nW; j+=2)
+                    { 
+
+                        image3[i][j].r= image[i][j].r;
+                        image3[i][j].b= image[i][j].b;
+
+                    }
+
+                }
+                for (int i=0; i < nH; i+=2)
+                {
+                    for (int j=0; j < nW; j+=2)
+                    { 
+
+                        for(int u = 0 ; u<2;u++)
+                            for(int v =0 ; v<2;v++)
+                            {
+                                image3[i+u][j+v].r= image[i][j].r;
+                                image3[i+u][j+v].b= image[i][j].b;
+                            }
+
+                    }
+
+                }
+                for (int i=0; i < nH; i++)
+                {
+                    for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                    {
+                        ImgGB[i *tnW +p  ]=image3[i][j].r;
+                        ImgGB[i *tnW +p+1]=0;
+                        ImgGB[i *tnW +p+2]=image3[i][j].b;
+                    }
+                }
+                            for (int i=0; i < nH; i++)
+            {
+                for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                {
+                    ImgOut[i *tnW +p  ]=image3[i][j].r;
+                    ImgOut[i *tnW +p+1]=image2[i][j].g;
+                    ImgOut[i *tnW +p+2]=image3[i][j].b;
+                }
+            }
+        break;
+        case 3:
+                for (int i=0; i < nH; i++)
+                {
+                    for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                    {
+                        image[i][j].r=ImgIn[i *tnW +p  ];
+                        image[i][j].g=ImgIn[i *tnW +p+1];
+                        image[i][j].b=ImgIn[i *tnW +p+2];
+
+
+                        image2[i][j].b=ImgIn[i *tnW +p +2];
+
+                    }
+                }
+                // SUBSAMBLING RGB
+                
+
+                for (int i=0; i < nH; i+=2)
+                {
+                    for (int j=0; j < nW; j+=2)
+                    { 
+
+                        image3[i][j].r= image[i][j].r;
+                        image3[i][j].g= image[i][j].g;
+
+                    }
+
+                }
+                for (int i=0; i < nH; i+=2)
+                {
+                    for (int j=0; j < nW; j+=2)
+                    { 
+
+                        for(int u = 0 ; u<2;u++)
+                            for(int v =0 ; v<2;v++)
+                            {
+                                image3[i+u][j+v].r= image[i][j].r;
+                                image3[i+u][j+v].g= image[i][j].g;
+                            }
+
+                    }
+
+                }
+                for (int i=0; i < nH; i++)
+                {
+                    for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                    {
+                        ImgGB[i *tnW +p  ]=image3[i][j].r;
+                        ImgGB[i *tnW +p+1]=image3[i][j].g;
+                        ImgGB[i *tnW +p+2]=0;
+                    }
+                }
+            for (int i=0; i < nH; i++)
+            {
+                for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
+                {
+                    ImgOut[i *tnW +p  ]=image3[i][j].r;
+                    ImgOut[i *tnW +p+1]=image3[i][j].g;
+                    ImgOut[i *tnW +p+2]=image2[i][j].b;
+                }
+            }
+        break;
     }
-    // END SUBSAMBLING RGB
-    for (int i=0; i < nH; i++)
-    {
-        for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
-        {
-            ImgOut[i *tnW +p  ]=image2[i][j].r;
-            ImgOut[i *tnW +p+1]=image3[i][j].g;
-            ImgOut[i *tnW +p+2]=image3[i][j].b;
-        }
-    }
-    for (int i=0; i < nH; i++)
-    {
-        for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
-        {
-            ImgGB[i *tnW +p  ]=0;
-            ImgGB[i *tnW +p+1]=image3[i][j].g;
-            ImgGB[i *tnW +p+2]=image3[i][j].b;
-        }
-    }
-        ecrire_image_ppm(pathOut, ImgOut,  nH, nW);
-    char * pathGB = makePath((char*)"gbsub.ppm",folderOut);
+
+
+
+    char * pathRGB = makePath((char*)"rgb.ppm",folderOut);
+    
+    ecrire_image_ppm(pathRGB, ImgOut,  nH, nW);
+    char * pathGB = makePath((char*)"2composantes.ppm",folderOut);
     ecrire_image_ppm(pathGB, ImgGB,  nH, nW);
     printf("PSNR : %f\n",psnr(ImgIn,ImgOut, nH,  nW));
 
 
-    }
-    else
-    {
+
         //image 2 luminance
         //image3 chromian
-
-
-        int tnW = 3*nW;
         for (int i=0; i < nH; i++)
         {
             for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
@@ -251,9 +329,10 @@ int main(int argc, char* argv[]) {
 
         }
         // SUBSAMPLING
-        for (int i=0; i < nH; i+=2)
+        int sampling =2;
+        for (int i=0; i < nH; i+=sampling)
         {
-            for (int j=0; j < nW; j+=2)
+            for (int j=0; j < nW; j+=sampling)
             { 
 
                 image3[i][j].r= image3[i][j].r;
@@ -262,13 +341,13 @@ int main(int argc, char* argv[]) {
             }
 
         }
-            for (int i=0; i < nH; i+=2)
+            for (int i=0; i < nH; i+=sampling)
         {
-            for (int j=0; j < nW; j+=2)
+            for (int j=0; j < nW; j+=sampling)
             { 
 
-                for(int u = 0 ; u<2;u++)
-                    for(int v =0 ; v<2;v++)
+                for(int u = 0 ; u<sampling;u++)
+                    for(int v =0 ; v<sampling;v++)
                     {
                         image3[i+u][j+v].r= image3[i][j].r;
                         image3[i+u][j+v].b= image3[i][j].b;
@@ -289,38 +368,37 @@ int main(int argc, char* argv[]) {
             }
         }
          ecrire_image_ppm(pathLumi, ImgOut,  nH, nW);
-        /*
+        
         char * pathChromi = makePath((char*)"chrominance.ppm",folderOut);
         for (int i=0; i < nH; i++)
         {
             for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
             {
-                ImgTrans[i *tnW +p  ]=image3[i][j].r;
-                ImgTrans[i *tnW +p+1]=image3[i][j].g;
-                ImgTrans[i *tnW +p+2]=image3[i][j].b;
+                ImgGB[i *tnW +p  ]=image3[i][j].r;
+                ImgGB[i *tnW +p+1]=image3[i][j].g;
+                ImgGB[i *tnW +p+2]=image3[i][j].b;
             }
         }
-         ecrire_image_ppm(pathChromi, ImgTrans,  nH, nW);
-         */
-        
-        char * pathChromi = makePath((char*)"sard.ppm",folderOut);
+         ecrire_image_ppm(pathChromi, ImgGB,  nH, nW);
+         
+
         for (int i=0; i < nH; i++)
         {
             for (int j=0, p=0; j < nW , p<tnW; j++,p+=3)
             {
-                int r = image3[i][j].r*1.402 + image2[i][j].r;
-                int b = image3[i][j].b*1.772 + image2[i][j].r;
-                int g =  (image2[i][j].r -r*0.299 -b*0.114)/0.587;
+                float r =  std::min(255.,std::max(0.,image3[i][j].r*1.402 + image2[i][j].r));
+                float b =  std::min(255.,std::max(0.,image3[i][j].b*1.772 + image2[i][j].r));
+                float g =  std::min(255.,std::max(0.,(image2[i][j].r -r*0.299 -b*0.114)/0.587));
 
-                ImgTrans[i *tnW +p  ]=r;
-                ImgTrans[i *tnW +p+1]=g;
-                ImgTrans[i *tnW +p+2]=b;
+                ImgTrans[i *tnW +p  ]=(int)r;
+                ImgTrans[i *tnW +p+1]=(int)g;
+                ImgTrans[i *tnW +p+2]=(int)b;
             }
         }
-         ecrire_image_ppm(pathChromi, ImgTrans,  nH, nW);
+         ecrire_image_ppm(pathOut, ImgTrans,  nH, nW);
          printf("PSNR : %f\n",psnr(ImgIn,ImgTrans, nH,  nW));
 
-    }
+    
 
 
     free(ImgIn);
