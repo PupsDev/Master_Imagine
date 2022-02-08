146e9eeafbe7040d89681d29199c6d2662de47a7
PupsDev <Control3d@gmail.com>
2022-02-08 12:10:51 +0100

    tp codage ondelette

--- S2/HAI809I Codage et compression multimédia/tp4/src/main.cpp
+++ S2/HAI809I Codage et compression multimédia/tp4/src/main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <cassert>
 #include <math.h>
+#include <bitset>
 
 struct couleur
 {
@@ -139,14 +140,21 @@ void resize(ImageG& image, int nW,int nH)
     for(auto & line: image)
         line.resize(nW);
 }
-void ondelette1D(ImageG &data, int nW,int nH)
+void ondelette1D(ImageG &data, int nW,int nH, double Q1, double Q2)
 {
     ImageG out;
     resize(out,nW,nH);
 
-    for (size_t i = 0; i < nH; i+=2)
+    ImageG BF,MFV,MFH,HF;
+    resize(BF,nW/2,nH/2);
+    resize(MFV,nW/2,nH/2);
+    resize(MFH,nW/2,nH/2);
+    resize(HF,nW/2,nH/2);
+
+    std::cout<<nW<<nH<<std::endl;
+    for (size_t i = 0; i < nH-1; i++)
 	{
-		for (size_t j = 0; j < nW; j+=2)
+		for (size_t j = 0; j < nW-1; j++)
 		{
 			double A = data[i][j];
 			double B = data[i][j+1];
@@ -158,11 +166,20 @@ void ondelette1D(ImageG &data, int nW,int nH)
 			double Z = double(A - B + C - D) / 2;
 			double O = double(A - B - C + D);
 
-			out[(i / 2)][(j / 2)] = X;
+            O = 1./Q1 * O;
+            Y = 1./Q2 * Y;
+            Z = 1./Q2 * Z;
 
-			out[(i / 2)][nW / 2 + (j / 2)] = Y;
-			out[nH / 2 + (i / 2)][(j / 2)] = Z;
-			out[nH / 2 + (i / 2)][nW / 2 + (j / 2)] = O;
+            out[(i / 2)][(j / 2)] =                     std::max( std::min(255.,X),0.);
+			out[(i / 2)][nW / 2 + (j / 2)] =            std::max( std::min(255.,Y+128.),0.);
+			out[nH / 2 + (i / 2)][(j / 2)] =            std::max( std::min(255.,Z+128.),0.);
+			out[nH / 2 + (i / 2)][nW / 2 + (j / 2)] =   std::max( std::min(255.,O+128.),0.);
+            
+            BF[i/2][j/2]= std::max( std::min(255.,X),0.);
+            MFV[i/2][j/2]=std::max( std::min(255.,Y+128.),0.);
+            MFH[i/2][j/2]=std::max( std::min(255.,Z+128.),0.);
+            HF [i/2][j/2]=std::max( std::min(255.,O+128.),0.);
+
 		}
 	}
         for (size_t i = 0; i < nH; i++)
@@ -172,11 +189,15 @@ void ondelette1D(ImageG &data, int nW,int nH)
             data[i][j]=out[i][j];
         }
     }
+    char * folderOut = (char*)"out/"; 
+    char * inputName = (char*)"lena256.pgm"; 
+    saveImage(makeFinalPath( folderOut, (char*)"BF",inputName), nH/2,nW/2,BF);
+    saveImage(makeFinalPath( folderOut, (char*)"MFV_",inputName), nH/2,nW/2,MFV);
+    saveImage(makeFinalPath( folderOut, (char*)"MFH_",inputName), nH/2,nW/2,MFH);
+    saveImage(makeFinalPath( folderOut, (char*)"HF_",inputName), nH/2,nW/2,HF);
 }
-void Iondelette1D(ImageG &data, int nW,int nH)
-{
-    
-}
+
+/*
 void ondelette(ImageG &luminance,ImageG &Cr,ImageG &Cb, int nW,int nH)
 {
     ondelette1D(luminance,nW,nH);
@@ -195,8 +216,89 @@ void ondelette(ImageG &luminance,ImageG &Cr,ImageG &Cb, int nW,int nH)
 
 
 
-}
+}*/
+void ondelette2(ImageG &image, int nW,int nH, double Q1, double Q2)
+{
+    ondelette1D(image,nW,nH,Q1, Q2);
+    /*
+    int N = 1;
+    for(int i = 0 ; i < N-1; i++)
+    {
 
+        ondelette1D(image,nW,nH,  Q1, Q2);
+        nW/=2;
+        nH/=2;
+    }
+    */
+}
+/*
+void write(ostream& str, const bitset& bs)
+{
+    while( bits_remaining_in_bs )
+    {
+        int bitstoextract = 8 - bitswritten;
+
+        bitbuffer |= extract_bits_from_bitset( bs, bitstoextract );
+        
+        if(bitswritten == 8)
+        {
+            str.write(&bitbuffer,1);
+            bitswritten = 0;
+            bitbuffer = 0;
+        }
+    }
+}
+*/
+void codageParPlage(ImageG &image,char * out ,int nW,int nH)
+{
+    int plage = 5; // 5 bits par plage 
+    int i=0;
+    std::string codage;
+    while(i< (nH*nH)-1 )
+    {
+        int px = image[i/nW][i%nW];
+        int counter = 1;
+        do
+        {
+            i++;
+            counter++;
+        }
+        while(px == image[i/nW][i%nW]&&counter<pow(2,plage) );
+        std::string binary = std::bitset<8>(px).to_string();
+        std::string counterString = std::bitset<5>(counter).to_string();
+        codage+=binary+counterString;
+        i++;
+
+    }
+    std::cout<<codage;
+    for(int i =0; i < codage.size();i++)
+    {
+        std::string mybitset;
+        for(int j = 0 ; j <8;j++)
+            mybitset+=codage[i];
+        std::bitset<8> octet(mybitset); 
+    }
+    //for (size_t i = 0; i < nH*nW; i++)
+	//{
+		//for (size_t j = 0; j < nW; j++)
+		//{
+            //int px = image[i/nW][i%nW];
+            //int bit =0;
+            // 5 bits 32 valeurs
+            // 0 - 255 -> 8 bits
+            //std::cout<<px<<"\n";
+            //std::string binary = std::bitset<8>(px).to_string();
+            /*
+            int count = 1;
+            while (i < n - 1 && str[i] == str[i + 1]) {
+                count++;
+                i++;
+            }
+            */
+        //}
+    //}
+
+}
 int main(int argc, char* argv[]) {
     char inputName[250];
     int nH, nW;
@@ -212,7 +314,7 @@ int main(int argc, char* argv[]) {
     sscanf (argv[1],"%s",inputName) ;
 
     char * pathIn = makePath(inputName,folderIn);
-
+    /*
     ImageRGB imageRGB;
 
     ImageG luminance;
@@ -241,7 +343,16 @@ int main(int argc, char* argv[]) {
     saveImage(makeFinalPath2( folderOut, (char*)"luminance_2",inputName), nH,nW,luminance);
     saveImage(makeFinalPath2( folderOut, (char*)"Cr_2",inputName), nH,nW,Cr);
     saveImage(makeFinalPath2( folderOut, (char*)"Cb_2",inputName), nH,nW,Cb);
-    
+    */
+    ImageG image;
+    double Q1 = 5.,Q2=5.;
+    lire_nb_lignes_colonnes_image_pgm(pathIn, &nH, &nW);
+    resize(image,nW,nH);
+    loadImage(pathIn,image,nW, nH);
+    ondelette2(image,nW,nH,Q1,Q2);
+    codageParPlage(image,(char*)"out",nW,nH);
+    saveImage(makeFinalPath( folderOut, (char*)"originale_",inputName), nH,nW,image);
+
 
 
 
